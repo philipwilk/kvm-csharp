c724cf650210b9870eba6488521cf16c1f6e3eef
Philip Wilk <wiryfuture@gmail.com>
2022-08-14 11:33:08 +0100

    remove comment

--- ioctls/consts.h
+++ ioctls/consts.h
@@ -1,7 +1,3 @@
-/*
-  I think all executables need the naming convention lib(WHATEVER).so
-  Then when importing it in csharp you import it as (WHATEVER).so
-*/
 // I know i could import <sys/kvm.h> but that would be too easy
 // KVM ioctl id
 const int KVM_ID = 0xAE;
