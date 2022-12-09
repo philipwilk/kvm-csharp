996f2710f8408376a7461350b778f95db55498eb
Philip Wilk <wiryfuture@gmail.com>
2022-12-09 15:57:55 +0000

    info

--- src/exceptions/sql.cs
+++ src/exceptions/sql.cs
@@ -2,6 +2,6 @@ namespace Main
 {
   public class no_db_details : ArgumentException
   {
-    public no_db_details() : base("Sql database user and password not provided.") { }
+    public no_db_details() : base("Sql database user and password not provided, pass them using sqlUser and sqlPassword") { }
   }
 }
